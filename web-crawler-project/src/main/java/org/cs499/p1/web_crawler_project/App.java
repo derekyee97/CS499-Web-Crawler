6c6ec4c17e30e56c30a6396309ad6761b899bf25
Derek Yee <dereky007@gmail.com>
2019-03-07 16:35:51 -0800

    converted to maven project, updated pom xml for jsoup dependency

---
+++ web-crawler-project/src/main/java/org/cs499/p1/web_crawler_project/App.java
@@ -0,0 +1,13 @@
+package org.cs499.p1.web_crawler_project;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ web-crawler-project/src/main/java/org/cs499/p1/web_crawler_project/Web_Crawler.java
@@ -0,0 +1,6 @@
+package org.cs499.p1.web_crawler_project;
+import org.jsoup.Jsoup;
+public class Web_Crawler
+{
+	
+}
