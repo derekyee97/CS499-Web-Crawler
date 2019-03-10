dffb387be75fc95dd997a7a14b2c4a833f65e724
Derek Yee <dereky007@gmail.com>
2019-03-09 22:43:09 -0800

    Completed crawler
    Run App.java to activate crawler.
    Place a URL inside the App.java class to test
    
    Web_Crawler class will take link and go to URL location. It will
    download html and create txt file on your cpu under the resource folder
    of your git project. It will create a seperate document for each html
    page downloaded (I maxed out the pages to crawl to 100 but for testing
    purposes you might want to do smaller)

--- web-crawler-project/src/main/java/org/cs499/p1/web_crawler_project/App.java
+++ web-crawler-project/src/main/java/org/cs499/p1/web_crawler_project/App.java
@@ -1,5 +1,7 @@
 package org.cs499.p1.web_crawler_project;
 
+import java.io.File;
+
 /**
  * Hello world!
  *
@@ -8,6 +10,10 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Bye!" );
+       Web_Crawler crawler=new Web_Crawler();  
+       crawler.crawl("https://www.oracle.com/java/",0);
+       System.out.println("done");
+      // File file=new File("C:\\Users\\derek\\git\\CS499-Web-Crawler\\web-crawler-project\\src\\main\\Resources"); //specific path, do not know how to make it flexible
+      
     }
 }
--- web-crawler-project/src/main/java/org/cs499/p1/web_crawler_project/Web_Crawler.java
+++ web-crawler-project/src/main/java/org/cs499/p1/web_crawler_project/Web_Crawler.java
@@ -1,6 +1,77 @@
 package org.cs499.p1.web_crawler_project;
-import org.jsoup.Jsoup;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
+import java.util.HashSet;
+import org.jsoup.*;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 public class Web_Crawler
 {
+	private HashSet<String> visited;//will keep track of websites we visited (dont want to revisit)
+	//private HashSet<String> frontier;
+	private int maxPages;
+	private String fileName;
+	private int docNumber;
+	public Web_Crawler()
+	{
+		visited=new HashSet<String>();
+		//frontier=new HashSet<String>();
+		maxPages=100;
+		fileName="document";
+		docNumber=0;
+	}
+	public void setMaxPages(int pages)
+	{
+		maxPages=pages;
+		
+	}
+	public void crawl(String websiteURL, int max)
+	{
+		if(max==maxPages)
+		{
+			return;
+		}
+		else if(!visited.contains(websiteURL))
+		{
+			String nameFile=fileName+Integer.toString(docNumber)+".txt";   //creating new file to write html for EACH link
+			try 
+			{
+				File file=new File("src\\main\\Resources\\"+nameFile);
+				PrintWriter writer=new PrintWriter(file,"UTF-8");
+				
+				docNumber++; //gives each link its own txt file name
+				
+				visited.add(websiteURL);  //add to visited
+				Document webPage=Jsoup.connect(websiteURL).get(); //get the html code corresponding to webpage of url
+				writer.write(webPage.toString());  //writing html to a file
+			//	System.out.print(webPage.toString());  //prints out html for you to test
+				Elements otherLinks=webPage.select("a[href]");  //get links on that page
+				System.out.println(visited.size());
+				
+				for(Element pageToVisit: otherLinks)
+				{
+					crawl(pageToVisit.attr("abs:href"),visited.size());  //this loop will go visit other urls.
+				}
+				
+				writer.close();
+			} 
+			
+			catch (IOException e)    
+			{
+				System.out.println("Error: "+websiteURL);
+			}
+			catch(IllegalArgumentException ex) //will capture anything that is NOT a link to another webpage (instead links to a picture for example, and ignores it)
+			{
+				
+				return;
+			}
+		}
+		
+	}
+	
 	
 }
