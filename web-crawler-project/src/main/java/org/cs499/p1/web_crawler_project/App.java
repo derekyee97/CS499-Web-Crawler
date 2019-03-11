be4432f46dcb6b2676fb02fb022f8705b2cbf313
Derek Yee <dereky007@gmail.com>
2019-03-10 19:52:27 -0700

    changed so that it does not create new text files but stores documents in hashmap

--- web-crawler-project/src/main/java/org/cs499/p1/web_crawler_project/App.java
+++ web-crawler-project/src/main/java/org/cs499/p1/web_crawler_project/App.java
@@ -1,6 +1,10 @@
 package org.cs499.p1.web_crawler_project;
 
 import java.io.File;
+import java.util.HashMap;
+import java.util.Set;
+
+import org.jsoup.nodes.Document;
 
 /**
  * Hello world!
@@ -12,8 +16,15 @@ public class App
     {
        Web_Crawler crawler=new Web_Crawler();  
        crawler.crawl("https://www.oracle.com/java/",0);
-       System.out.println("done");
-      // File file=new File("C:\\Users\\derek\\git\\CS499-Web-Crawler\\web-crawler-project\\src\\main\\Resources"); //specific path, do not know how to make it flexible
+       HashMap<String,Document> result=crawler.getDocuments();
+       
+//      for(String key:result.keySet())
+//      {
+//    	  System.out.println(result.get(key).toString());
+//      }
+       
+       
+     
       
     }
 }
--- web-crawler-project/src/main/java/org/cs499/p1/web_crawler_project/Web_Crawler.java
+++ web-crawler-project/src/main/java/org/cs499/p1/web_crawler_project/Web_Crawler.java
@@ -4,6 +4,7 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.io.UnsupportedEncodingException;
+import java.util.HashMap;
 import java.util.HashSet;
 import org.jsoup.*;
 import org.jsoup.nodes.Document;
@@ -12,17 +13,19 @@ import org.jsoup.select.Elements;
 public class Web_Crawler
 {
 	private HashSet<String> visited;//will keep track of websites we visited (dont want to revisit)
-	//private HashSet<String> frontier;
 	private int maxPages;
 	private String fileName;
 	private int docNumber;
+	private HashMap<String,Document> documentsHolder;
 	public Web_Crawler()
+	
 	{
 		visited=new HashSet<String>();
-		//frontier=new HashSet<String>();
-		maxPages=100;
+		documentsHolder=new HashMap<String,Document>();
+		maxPages=11;
 		fileName="document";
 		docNumber=0;
+		
 	}
 	public void setMaxPages(int pages)
 	{
@@ -37,27 +40,21 @@ public class Web_Crawler
 		}
 		else if(!visited.contains(websiteURL))
 		{
-			String nameFile=fileName+Integer.toString(docNumber)+".txt";   //creating new file to write html for EACH link
+			String nameFile=fileName+Integer.toString(docNumber);//+".txt";   //creating new file to write html for EACH link
 			try 
 			{
-				File file=new File("src\\main\\Resources\\"+nameFile);
-				PrintWriter writer=new PrintWriter(file,"UTF-8");
-				
-				docNumber++; //gives each link its own txt file name
-				
 				visited.add(websiteURL);  //add to visited
 				Document webPage=Jsoup.connect(websiteURL).get(); //get the html code corresponding to webpage of url
-				writer.write(webPage.toString());  //writing html to a file
-			//	System.out.print(webPage.toString());  //prints out html for you to test
+				documentsHolder.put(nameFile,webPage);  
+				docNumber++; 				
 				Elements otherLinks=webPage.select("a[href]");  //get links on that page
-				System.out.println(visited.size());
-				
-				for(Element pageToVisit: otherLinks)
+						
+				for(Element pageToVisit: otherLinks) //iterate through links listed on webpage
 				{
 					crawl(pageToVisit.attr("abs:href"),visited.size());  //this loop will go visit other urls.
 				}
 				
-				writer.close();
+				//writer.close();
 			} 
 			
 			catch (IOException e)    
@@ -71,6 +68,11 @@ public class Web_Crawler
 			}
 		}
 		
+	}
+	public HashMap<String,Document> getDocuments()
+	{
+		HashMap<String,Document> temp=(HashMap<String, Document>) documentsHolder.clone();//send copy of this
+		return temp;
 	}
 	
 	
