dcefb9f0d7081b1284ec82925d98d6d89974857a
Derek Yee <dereky007@gmail.com>
2019-03-11 21:35:56 -0700

    added links to csv file as well as download all txt documents into repository folder

--- web-crawler-project/src/main/java/org/cs499/p1/web_crawler_project/App.java
+++ web-crawler-project/src/main/java/org/cs499/p1/web_crawler_project/App.java
@@ -1,6 +1,7 @@
 package org.cs499.p1.web_crawler_project;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Set;
 
@@ -12,10 +13,10 @@ import org.jsoup.nodes.Document;
  */
 public class App 
 {
-    public static void main( String[] args )
+    public static void main( String[] args ) throws IOException
     {
        Web_Crawler crawler=new Web_Crawler();  
-       crawler.crawl("https://en.wikipedia.org/wiki/Computer_science",0);
+       crawler.crawl("https://www.cosmopolitan.com/food-cocktails/a23440/health-benefits-dark-chocolate/",0);
        HashMap<String,Document> result=crawler.getDocuments();
        Zipf_Distribution zipf=new Zipf_Distribution(result);
        zipf.calculateFrequency();
--- web-crawler-project/src/main/java/org/cs499/p1/web_crawler_project/Web_Crawler.java
+++ web-crawler-project/src/main/java/org/cs499/p1/web_crawler_project/Web_Crawler.java
@@ -1,6 +1,7 @@
 package org.cs499.p1.web_crawler_project;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.io.UnsupportedEncodingException;
@@ -10,14 +11,18 @@ import org.jsoup.*;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+
+import au.com.bytecode.opencsv.CSVWriter;
 public class Web_Crawler
 {
 	private HashSet<String> visited;//will keep track of websites we visited (dont want to revisit)
 	private int maxPages;
 	private String fileName;
 	private int docNumber;
+	private File csvFile;
+	private CSVWriter csv;
 	private HashMap<String,Document> documentsHolder;
-	public Web_Crawler()
+	public Web_Crawler() throws IOException
 	
 	{
 		visited=new HashSet<String>();
@@ -25,8 +30,13 @@ public class Web_Crawler
 		maxPages=11;
 		fileName="document";
 		docNumber=0;
+		csvFile=new File("src\\main\\report.csv");
 		
+		csv=new CSVWriter(new FileWriter(csvFile));
+		csv.writeNext(new String[] {"URL","NumOutputLinks"});
+		csv.close();
 	}
+	
 	public void setMaxPages(int pages)
 	{
 		maxPages=pages;
@@ -40,11 +50,21 @@ public class Web_Crawler
 		}
 		else if(!visited.contains(websiteURL))
 		{
-			String nameFile=fileName+Integer.toString(docNumber);//+".txt";   //creating new file to write html for EACH link
+			String nameFile=fileName+Integer.toString(docNumber)+".txt";   //creating new file to write html for EACH link
+			
 			try 
 			{
+				int totalLinks=0;
+				String[] writeToCSV=new String[2]; //will only contain url and numbered links.
+				writeToCSV[0]=websiteURL;
+				File file=new File("src\\main\\Resources\\"+nameFile);
+				PrintWriter writer=new PrintWriter(file,"UTF-8");
+				CSVWriter writeCSV=new CSVWriter(new FileWriter(csvFile,true));
 				visited.add(websiteURL);  //add to visited
 				Document webPage=Jsoup.connect(websiteURL).get(); //get the html code corresponding to webpage of url
+				
+				writer.write(webPage.toString());  //writing html to a file
+				
 				documentsHolder.put(nameFile,webPage);  
 				docNumber++; 				
 				Elements otherLinks=webPage.select("a[href]");  //get links on that page
@@ -52,9 +72,13 @@ public class Web_Crawler
 				for(Element pageToVisit: otherLinks) //iterate through links listed on webpage
 				{
 					crawl(pageToVisit.attr("abs:href"),visited.size());  //this loop will go visit other urls.
+					totalLinks++;
+					System.out.println(totalLinks);
 				}
-				
-				//writer.close();
+				writeToCSV[1]=Integer.toString(totalLinks);
+				writeCSV.writeNext(writeToCSV);
+				writeCSV.close();
+				writer.close();
 			} 
 			
 			catch (IOException e)    
@@ -74,6 +98,5 @@ public class Web_Crawler
 		HashMap<String,Document> temp=(HashMap<String, Document>) documentsHolder.clone();//send copy of this
 		return temp;
 	}
-	
 	
 }
